fdb2ae28cff71a3273db1a0fb2a69906333b8044
brayquentin <brayquentin@gmail.com>
2024-04-18 09:56:16 +0200

    Update README and fix some issues
    Enforce cv2
    Move delay between resolution set and capture read

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# pibooth-gstreamer-camera
-Provide a way to configure the gstreamer camera command
+# pibooth-gstreamer-camera plugin
+Provide a way to configure an OpenCv camera using a custom GStreamer pipeline for [pibooth](https://github.com/pibooth/pibooth)
+
+### Initial issue
+This plugin has been developed following issue using a C790 HDMI adapter for my pibooth instance
+When using it with a GoPro camera and a resolution of 1920x180 at 50fps, I got a 'colorimetry 2:3:5:4 is not supported by your device' error
+
+After investigation, it seems that pibooth uses the default OpenCv configuration with the device meaning to implicitly use a RGB3 colorimetry when the device is streaming with a BT601 colorimetry
+
+Second issue was regarding timing, captures was taken to early after setting the resolution, this added a big green line in the bottom of the photo
+
+This implies to have a custom GStreamer pipeline used with OpenCv and custom delay
+
+### Installation
+To install this plugin, you can simply use pip. pibooth will automatically enable discover and enable it
+```
+pip install git+https://github.com/poupounetjoyeux/pibooth-gstreamer-camera.git@v1.0.0
+```
+
+### Configuration
+Search for your working GStreamer pipeline reagarding your device
+You can directly test it using command line without starting pibooth until you find the right settings
+Don't hesitate to read the [GStreamer plugins documentation](https://gstreamer.freedesktop.org/documentation/plugins_doc.html)
+
+For the C790, the working GStreamer pipeline for me is
+```
+v4l2src ! video/x-raw,width=1920,height=1080,framerate=50/1,format=UYVY,colorimetry=bt601 ! videoconvert ! appsink drop=1
+```
+
+Update your pibooth config like this
+```
+[CAMERA]
+
+# The full GStreamer pipeline. Don't forget to append 'appsink drop=1' at the end of the pipeline
+gstreamer_pipeline = v4l2src ! video/x-raw,width=1920,height=1080,framerate=50/1,format=UYVY,colorimetry=bt601 ! videoconvert ! appsink drop=1
+
+# Must be greater than 0.5s to ensure OpenCv will have the time to resize the capture frame. By default or if lower, will use 0.5s
+smile_display_delay = 0.5
+```
+**Don't forget to add 'appsink drop=1' at the end of your pipeline. This is mandatory to make it work correctly with OpenCv**
+
+### Thanks
+Thanks to the [pibooth](https://github.com/pibooth/pibooth) team that make a really great and amazing job!
+
--- pibooth_gstreamer_camera.py
+++ pibooth_gstreamer_camera.py
@@ -1,36 +1,68 @@
-"""Plugin to handle camera that needs custom GStreamer command to work with OpenGL"""
-import os
+"""Plugin to handle camera that needs custom GStreamer pipeline to work with OpenGL"""
+import time
 import pibooth
 from pibooth.utils import LOGGER
 from pibooth.camera.opencv import CvCamera
-try:
-    import cv2
-except ImportError:
-    cv2 = None  # OpenCV is optional
+import cv2
 
 __version__ = "1.0.0"
 
+class CvCameraWithDelay(CvCamera):
+
+    def __init__(self, camera_proxy, smile_display_delay):
+        super(CvCameraWithDelay, self).__init__(camera_proxy)
+        self.smile_display_delay = smile_display_delay
+
+    def capture(self, effect=None):
+        """Capture a new picture.
+        """
+        effect = str(effect).lower()
+        if effect not in self.IMAGE_EFFECTS:
+            raise ValueError("Invalid capture effect '{}' (choose among {})".format(effect, self.IMAGE_EFFECTS))
+
+        self._cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.resolution[0])
+        self._cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
+
+        if self.capture_iso != self.preview_iso:
+            self._cam.set(cv2.CAP_PROP_ISO_SPEED, self.capture_iso)
+
+        time.sleep(self.smile_display_delay)  # To let time to see "Smile"
+        LOGGER.debug("Taking capture at resolution %s", self.resolution)
+        ret, image = self._cam.read()
+        if not ret:
+            raise IOError("Can not capture frame")
+        image = self._rotate_image(image, self.capture_rotation)
+
+        LOGGER.debug("Putting preview resolution back to %s", self._preview_resolution)
+        self._cam.set(cv2.CAP_PROP_FRAME_WIDTH, self._preview_resolution[0])
+        self._cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self._preview_resolution[1])
+
+        if self.capture_iso != self.preview_iso:
+            self._cam.set(cv2.CAP_PROP_ISO_SPEED, self.preview_iso)
+
+        self._captures.append((image, effect))
+
+        self._hide_overlay()  # If stop_preview() has not been called
+
 @pibooth.hookimpl
 def pibooth_configure(cfg):
-    """Declare the new configuration options."""
+    """Declare the new configuration options"""
     cfg.add_option('CAMERA', 'gstreamer_pipeline', '', "The full GStreamer pipeline. Don't forget to append 'appsink drop=1' at the end of the pipeline")
-    cfg.add_option('CAMERA', 'gstreamer_prepare_commands', '', "A set of shell command to execute before starting the GStreamer pipeline")
-
+    cfg.add_option('CAMERA', 'smile_display_delay', 0.5, " Must be greater than 0.5s to ensure OpenCv will have the time to resize the capture frame. By default or if lower, will use 0.5s")
+    
 @pibooth.hookimpl
 def pibooth_setup_camera(cfg):
     gstreamer_pipeline = cfg.get('CAMERA', 'gstreamer_pipeline')
     if not gstreamer_pipeline:
-        raise EnvironmentError("No GStreamer pipeline defined")
-
-    sh_commands = cfg.gettuple('CAMERA', 'gstreamer_prepare_commands', str)
-    if sh_commands:
-        for sh_command in sh_commands:
-            os.system(sh_command)
-            LOGGER.info(f"Preparing camera with shell : {sh_command} ...")
+        raise EnvironmentError("No GStreamer pipeline defined, please update the pibooth configuration by adding  in the [CAMERA] section")
     
+    smile_display_delay = cfg.getfloat('CAMERA', 'smile_display_delay')
+    if not smile_display_delay:
+        smile_display_delay = 0.5
+ 
     cv_camera = cv2.VideoCapture(gstreamer_pipeline)
     if cv_camera.isOpened():
-        LOGGER.info(f"Configuring camera using custom GStreamer pipeline : {gstreamer_pipeline} ...")
-        return CvCamera(cv_camera)
+        LOGGER.info(f"Configuring OpenCv camera using GStreamer pipeline : {gstreamer_pipeline} ...")
+        return CvCameraWithDelay(cv_camera, smile_display_delay)
         
-    raise EnvironmentError(f"Unable to instantiate the custom GStreamer camera using : {gstreamer_pipeline}")
+    raise EnvironmentError(f"Unable to instantiate the OpenCv camera using GStreamer pipeline : {gstreamer_pipeline}")
